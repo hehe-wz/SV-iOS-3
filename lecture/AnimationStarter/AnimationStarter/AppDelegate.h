fbe1e12cfe2bc7620a65278c628b2168a267ba5d
Zun Wang <wangzun1988@gmail.com>
2015-12-04 22:13:54 -0800

    Animation Starter

---
+++ lecture/AnimationStarter/AnimationStarter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationStarter
+//
+//  Created by Zun Wang on 12/4/15.
+//  Copyright © 2015 ZW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lecture/AnimationStarter/AnimationStarter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationStarter
+//
+//  Created by Zun Wang on 12/4/15.
+//  Copyright © 2015 ZW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
